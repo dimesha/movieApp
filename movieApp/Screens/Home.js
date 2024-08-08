bb820b0346c14e475530087dc67da9e2e3503940
dimesha <dimesha99kanchani@gmail.com>
2024-08-08 13:10:09 +0530

    complete list

--- movieApp/Screens/Home.js
+++ movieApp/Screens/Home.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import { View, Text } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Movies from './Movies';
 import TvShows from './TvShows';
+import { Ionicons } from '@expo/vector-icons'; // Use @expo/vector-icons for Expo
 
 const Tab = createBottomTabNavigator();
 
-export default function Home() {
+export default function App() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Movies" component={Movies} options={{headerShown: false}}/>
-      <Tab.Screen name="TvShows" component={TvShows} options={{headerShown: false}} />
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Movies') {
+            iconName = focused ? 'film' : 'film-outline';
+          } else if (route.name === 'TvShows') {
+            iconName = focused ? 'tv' : 'tv-outline';
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+      })}
+    >
+      <Tab.Screen name="Movies" component={Movies} options={{ headerShown: false }} />
+      <Tab.Screen name="TvShows" component={TvShows} options={{ headerShown: false }} />
     </Tab.Navigator>
-    
-  )
+  );
 }
